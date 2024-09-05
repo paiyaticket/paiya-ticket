8b77976ecc2e4a41dfe9a55ce7d44f015890cb6c
koffiange <koffiange62@gmail.com>
2024-09-05 16:05:03 -0400

    feat : add first initial Empty component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { EmptyComponent } from './component/empty/empty.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path : "", component: EmptyComponent}
+];
