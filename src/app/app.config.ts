23b1c46f8e538f94c7a7621396ad3e3f6970ddff
koffiange <koffiange62@gmail.com>
2024-09-06 12:25:22 -0400

    feat : add angular/fire to the project.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,15 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimations()]
+  providers: [
+    provideRouter(routes), 
+    provideClientHydration(), 
+    provideAnimations(), 
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)), 
+    provideAuth(() => getAuth())]
 };
