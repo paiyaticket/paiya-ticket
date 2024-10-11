837657c35e427cbb0277a22587d8e7da189aa7f9
koffiange <koffiange62@gmail.com>
2024-10-11 11:06:45 -0400

    feat : add agenda form and save process (finished).

--- src/app/component/my-event/my-event-create/agenda/agenda-list/agenda-list.component.ts
+++ src/app/component/my-event/my-event-create/agenda/agenda-list/agenda-list.component.ts
@@ -1,13 +1,28 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { TimelineModule } from 'primeng/timeline';
+import { ButtonModule } from 'primeng/button';
+import { TimeSlot } from '../../../../../models/time-slot';
 
 @Component({
   selector: 'app-agenda-list',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    TimelineModule,
+    ButtonModule
+  ],
   templateUrl: './agenda-list.component.html',
   styleUrl: './agenda-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AgendaListComponent {
+
+    @Input() timeSlots : TimeSlot[] | undefined;
+
+
+    ngOnInit(){
+        console.log(this.timeSlots);
+    }
 
 }
