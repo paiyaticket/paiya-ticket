5f890b3eab8075a347ab28628b887148124011c3
koffiange <koffiange62@gmail.com>
2024-12-27 20:21:20 -0500

    feat : ajouter la sélection de catégorie d'événement et améliorer les indications pour les mots clés

--- src/app/components/organizer-mode/my-event/my-event-create/my-event-create.component.ts
+++ src/app/components/organizer-mode/my-event/my-event-create/my-event-create.component.ts
@@ -52,6 +52,7 @@ import * as _ from 'lodash-es';
 import { AgendaUpdateComponent } from './agenda/agenda-update/agenda-update.component';
 import { OnlinePlatform } from '@enumerations/online-platform';
 import { PhysicalAddress } from '@models/physical-address';
+import { EVENT_CATEGORY_OPTIONS } from '@datas/event-category.data';
 
 
 
@@ -158,6 +159,7 @@ export class MyEventCreateComponent implements OnInit, OnDestroy {
         {'label' : $localize `Tiktok`, 'value' : OnlinePlatform.TIKTOK},
         {'label' : $localize `Zoom`, 'value' : OnlinePlatform.ZOOM},
     ];
+    eventCategoryOptions : any[] = EVENT_CATEGORY_OPTIONS;
 
     @Input() eventId : string | undefined;
     @ViewChild("agendaList") agendaList : AgendaListComponent | undefined;
---
+++ src/app/datas/event-category.data.ts
@@ -0,0 +1,12 @@
+export const EVENT_CATEGORY_OPTIONS: any[] = [
+    { label: $localize `Concert`, value: 'concert' },
+    { label: $localize `Festival`, value: 'festival' },
+    { label: $localize `Théâtre`, value: 'theater' },
+    { label: $localize `Cinema`, value: 'cinema' },
+    { label: $localize `Exposition/Salon`, value: 'exhibition' },
+    { label: $localize `Conférence`, value: 'conference' },
+    { label: $localize `Sport`, value: 'sport' },
+    { label: $localize `Sortie de groupe`, value: 'Sortie' },
+    { label: $localize `Pélérinage`, value: 'Pilgrimage' },
+    { label: $localize `Autre`, value: 'autre' }
+];
